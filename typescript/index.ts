966b55de8331c25795ee92726d48b6714338435f
Kasai Kou <80010105+streamwest-1629@users.noreply.github.com>
2023-02-13 17:38:44 +0900

    #3: debugModeを追加

--- typescript/index.ts
+++ typescript/index.ts
@@ -2,14 +2,17 @@ import { app } from "electron";
 import { execSync } from "child_process";
 import path from "path";
 
+const debugMode = app.isPackaged || process.env.NODE_ENV === "development";
 const resourcePath = app.isPackaged ? process.resourcesPath : ".";
 const pythonPath = path.join(resourcePath, ".venv/bin/python");
 
 async function main() {
-    console.log(`is packaged: ${app.isPackaged}`);
-    console.log(`resource path: ${resourcePath}`);
-    const buffer = await execSync(`${pythonPath} --version`);
-    console.log(`Python version:`, buffer.toString());
+    if (debugMode) {
+        console.log("THIS IS DEBUG MODE")
+        console.log(`resource path: ${resourcePath}`);
+        console.log(`Python version:`, await execSync(`${pythonPath} --version`).toString());
+    }
+
 }
 
 main().catch((e) => {
