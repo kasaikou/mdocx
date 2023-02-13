bb7fff72168f3410c13f3f1e9d3d24b34c97b14f
Kasai Kou <80010105+streamwest-1629@users.noreply.github.com>
2023-02-13 16:13:02 +0900

    #1: 除外ファイルを追加

--- forge.config.ts
+++ forge.config.ts
@@ -5,7 +5,10 @@ import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 
 const config: ForgeConfig = {
-  packagerConfig: {},
+  packagerConfig: {
+    ignore: /(\.devcontainer|\.github|node_modules|\.git|cmd|src|typescript|\.ts|poetry|\.toml|eslint)/,
+    extraResource: [".venv", "python"],
+  },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})]
 };
