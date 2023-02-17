5b2eb8c6f92584b6440927cbd83e6b60abb5dbde
Kasai Kou <80010105+streamwest-1629@users.noreply.github.com>
2023-02-17 13:54:03 +0900

    #13: Windows用にMaker-Wixを追加

--- forge.config.ts
+++ forge.config.ts
@@ -3,6 +3,7 @@ import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
+import { MakerWix } from '@electron-forge/maker-wix';
 import path from 'path';
 
 const config: ForgeConfig = {
@@ -17,9 +18,10 @@ const config: ForgeConfig = {
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({}),
-    new MakerZIP({}, ['darwin']),
+    new MakerZIP({}, ['darwin', 'windows', 'linux']),
     new MakerRpm({}),
     new MakerDeb({}),
+    new MakerWix({}),
   ],
 };
 
