fb0721703139993f61362b9604d5c476ebb7e2e9
Kasai Kou <80010105+streamwest-1629@users.noreply.github.com>
2023-02-17 16:44:40 +0900

    #8: テストで使用しているパスをCI上でも動かせるように相対パスに修正

--- typescript/parser/markdown.test.ts
+++ typescript/parser/markdown.test.ts
@@ -1,13 +1,19 @@
 import { ParseMarkdown } from './markdown';
 import child_process from 'child_process';
 import fs from 'fs';
+import path from 'path';
 
 test('example', function () {
   const doc = ParseMarkdown(
-    fs.readFileSync('/workspace/example/example.md', { encoding: 'utf-8' }),
-    '/workspace/example',
-    '/workspace/example/example.docx',
-    '/workspace/example/example-style.docx'
+    fs.readFileSync(
+      path.resolve(path.join(__dirname, '../../example/example.md')),
+      {
+        encoding: 'utf-8',
+      }
+    ),
+    path.resolve(path.join(__dirname, '../../example')),
+    path.resolve(path.join(__dirname, '../../example/example.docx')),
+    path.resolve(path.join(__dirname, '../../example/example-style.docx'))
   );
 
   const body = JSON.stringify(doc, null, 2);
